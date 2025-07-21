bc2df66b767ef3188d5325741ca2a73350c7fe18
henry <1151071+HenrYxZ@users.noreply.github.com>
2025-07-20 22:26:23 -0400

    part 4: ray class

--- Main.cpp
+++ Main.cpp
@@ -3,9 +3,48 @@
 
 #include "Vec3.h"
 #include "Color.h"
+#include "Ray.h"
+
+color ray_color(const ray& r) {
+  vec3 unit_direction = unit_vector(r.direction());
+  auto a = 0.5 * (unit_direction.y() + 1.0);
+  return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+}
+
 
 
 int main() {
+
+  // Image
+
+  auto aspect_ratio = 16.0 / 9.0;
+  int image_width = 400;
+
+  // Calculate the image height, and ensure that it's at least 1.
+  int image_height = int(image_width / aspect_ratio);
+  image_height = (image_height < 1) ? 1 : image_height;
+
+  // Camera
+
+  auto focal_length = 1.0;
+  auto viewport_height = 2.0;
+  auto viewport_width = viewport_height * (double(image_width) / image_height);
+  auto camera_center = point3(0, 0, 0);
+
+  // Calculate the vectors across the horizontal and down the vertical viewport edges.
+  auto viewport_u = vec3(viewport_width, 0, 0);
+  auto viewport_v = vec3(0, -viewport_height, 0);
+
+  // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+  auto pixel_delta_u = viewport_u / image_width;
+  auto pixel_delta_v = viewport_v / image_height;
+
+  // Calculate the location of the upper left pixel.
+  auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+  auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+
+	// Render
 	std::ofstream image_file("image.ppm");
 
 	int width = 256;
@@ -13,10 +52,14 @@ int main() {
 
 	// Write image headers
 	image_file << "P3\n" << width << ' ' << height << std::endl << "255\n";
+  point3 origin = point3();
 
 	for (int j = 0; j < height; j++) {
 		for (int i = 0; i < width; i++) {
-			auto pixel_color = color(double(i) / (width - 1), double(j) / (height - 1), 0);
+      vec3 pixel_loc = pixel00_loc + i * pixel_delta_u + j * pixel_delta_v;
+      vec3 direction = pixel_loc - origin;
+      ray r(origin, direction);
+			auto pixel_color = ray_color(r);
 			write_color(image_file, pixel_color);
 		}
 	}
