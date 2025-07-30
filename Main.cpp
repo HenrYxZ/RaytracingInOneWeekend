78ca07c738a18405704be1f33301a316e917ea73
henry <1151071+HenrYxZ@users.noreply.github.com>
2025-07-30 13:22:37 -0400

    fix: use calculated image height

--- Main.cpp
+++ Main.cpp
@@ -5,13 +5,33 @@
 #include "Color.h"
 #include "Ray.h"
 
+
+double hit_sphere(const point3& center, double radius, const ray& r) {
+  vec3 oc = center - r.origin();
+  auto a = dot(r.direction(), r.direction());
+  auto b = -2.0 * dot(r.direction(), oc);
+  auto c = dot(oc, oc) - radius * radius;
+  auto discriminant = b * b - 4 * a * c;
+  
+  if (discriminant < 0) {
+    return -1.0;
+  }
+  else {
+    return (-b - std::sqrt(discriminant)) / (2.0 * a);
+  }
+}
+
 color ray_color(const ray& r) {
+  auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+  if (t > 0.0) {
+    vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+    return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+  }
+
   vec3 unit_direction = unit_vector(r.direction());
   auto a = 0.5 * (unit_direction.y() + 1.0);
   return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
 }
-
-
 
 int main() {
 
@@ -47,18 +67,15 @@ int main() {
 	// Render
 	std::ofstream image_file("image.ppm");
 
-	int width = 256;
-	int height = 256;
-
 	// Write image headers
-	image_file << "P3\n" << width << ' ' << height << std::endl << "255\n";
-  point3 origin = point3();
+	image_file << "P3\n" << image_width << ' ' << image_height << std::endl << "255\n";
 
-	for (int j = 0; j < height; j++) {
-		for (int i = 0; i < width; i++) {
-      vec3 pixel_loc = pixel00_loc + i * pixel_delta_u + j * pixel_delta_v;
-      vec3 direction = pixel_loc - origin;
-      ray r(origin, direction);
+	for (int j = 0; j < image_height; j++) {
+    std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+		for (int i = 0; i < image_width; i++) {
+      vec3 pixel_loc = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+      vec3 direction = pixel_loc - camera_center;
+      ray r(camera_center, direction);
 			auto pixel_color = ray_color(r);
 			write_color(image_file, pixel_color);
 		}
